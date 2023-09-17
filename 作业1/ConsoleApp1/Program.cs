de7b9cd6b1b5866886d27070db728d8367606e01
cyb123lxx <127015988+cyb123lxx@users.noreply.github.com>
2023-09-17 19:34:01 +0800

    Add files via upload

---
+++ 作业1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿
+namespace myApp {
+class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入一组整数");
+            int length =
+                Convert.ToInt32(Console.ReadLine());
+            int[] arry = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                arry[i]=
+                    Convert.ToInt32(Console.ReadLine());}
+            for (int j = 0; j < length; j++)
+            {
+
+                for (int m = 2; m <= arry[j]; m++)
+                {
+                    if (arry[j] % m == 0)
+                        break;
+                    if (m == arry[j])
+                        Console.WriteLine(arry[j]);
+                }
+                                      
+            }
+        }
+    }
+
+
+}
